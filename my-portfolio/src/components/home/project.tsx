a44e123b7626a78bc60b5fb70278a8fc455f6d7f
Cameron Clay <cameronpclay@gmail.com>
2024-02-04 20:19:17 -0700

    Improved project formatting and tabing between elements

--- my-portfolio/src/components/home/project.tsx
+++ my-portfolio/src/components/home/project.tsx
@@ -19,7 +19,8 @@ export default function Project({
     const ref = useRef<HTMLDivElement>(null);
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ['0 1', '1.33 1'] //start when bottom of viewport reaches top of target, end when bottom of viewport has gone 33% past the project
+        // offset: ['0 1', '1.33 1'] //start when bottom of viewport reaches top of target, end when bottom of viewport has gone 33% past the project
+        offset: ['start end', '1.33 end'] //start when bottom of viewport reaches top of target, end when bottom of viewport has gone 33% past the project
     });
     const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
     const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
@@ -40,15 +41,17 @@ export default function Project({
             <section className='max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-[2rem] relative sm:h-[24rem] bg-gray-100 hover:bg-gray-200 transition dark:text-white dark:bg-white/[10%] dark:hover:bg-white/[15%]'>
                 <div className='flex flex-col h-full pt-[1rem] pb-[1.75rem] px-[1.25rem] sm:pl-[2.5rem] sm:pr-[0.5rem] sm:pt-[2.5rem] sm:max-w-[60%]'>
                     <h3 className='text-2xl font-semibold'>{title}</h3>
+                    <hr className='mt-[0.25rem]'/>
                     {
                         //whitespace-pre-line displays \n as a line break instead of as plain text
                     }
-                    <p className='mt-[0.5rem] leading-relaxed text-gray-700 dark:text-white/70 whitespace-pre-line'>{description}</p>
-
+                    <p className='mt-[0.75rem] leading-relaxed text-gray-700 dark:text-white/70 whitespace-pre-line'>{description}</p>
                     {
                         //mt-auto (auto means browser can select a suitable value)
                     }
-                    <ul className='flex flex-wrap mt-[1rem] gap-[0.5rem] sm:mt-auto'>
+                    <ul 
+                        className='flex flex-wrap mt-[1rem] gap-[0.5rem] sm:mt-auto'
+                    >
                         {
                             topSkills.map((tag, index) => (
                                 <li className=
@@ -60,7 +63,9 @@ export default function Project({
                             ))
                         }
                     </ul>
-                    <ul className='flex flex-wrap gap-[0.5rem] mt-[0.5rem]'>
+                    <ul 
+                        className='flex flex-wrap gap-[0.5rem] mt-[0.5rem]'
+                    >
                         {
                             otherSkills.map((tag, index) => (
                                 <li className=
@@ -88,7 +93,7 @@ export default function Project({
                             sm:hover:-translate-x-[45%]'
                 > */}
                 <button
-                    className='absolute w-full h-full top-[2rem] sm:-right-[7rem] sm:h-[auto] sm:max-w-[22.5rem]
+                    className='absolute w-full h-full top-[0rem] left-[0rem] sm:top-[2rem] sm:-right-[7rem] sm:left-[auto] sm:h-[auto] sm:max-w-[22.5rem]
                             transition
 
                             sm:hover:scale-[1.25]
@@ -98,6 +103,7 @@ export default function Project({
                         imageViewerContext.setImageSrc(fullImageUrl);
                         imageViewerContext.setVisible(true);
                     }}
+                    tabIndex={0}
                 >
                     <img
                         title='Click to open full image'
@@ -105,7 +111,7 @@ export default function Project({
                         alt={`Project ${title}`}
                         fetchPriority='high'
                         className='hidden sm:block w-full h-full py-[1rem]'
-                        tabIndex={0}
+                        tabIndex={-1}
                     />
                 </button>  
             </section>
