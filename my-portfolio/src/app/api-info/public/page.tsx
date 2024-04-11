8fbf921020d240a06cb452655bb30cdf17c74eff
Cameron <cameronpclay@gmail.com>
2024-04-11 16:22:32 -0600

    Minor refactoring

--- my-portfolio/src/app/api-info/public/page.tsx
+++ my-portfolio/src/app/api-info/public/page.tsx
@@ -5,7 +5,7 @@ import SectionContainer from '@src/components/api/section-container';
 import APIPublic from '@src/components/api/sections/api-public';
 import APIInfo from '@src/components/api/api-info';
 
-export default function AdminAPI() {
+export default function PublicAPI() {
     return (
         <SectionContainer>
             <APIInfo heading="Public API" description="REST API with JWT token Authentication."/>
