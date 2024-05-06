e94be06918368622a788b77f5bff276ca602a844
Cameron <cameronpclay@gmail.com>
2024-05-05 20:14:33 -0600

    Fixed test:ci command failing

--- my-portfolio/src/__tests__/unit-tests/layouts/api-layout.spec.tsx
+++ my-portfolio/src/__tests__/unit-tests/layouts/api-layout.spec.tsx
@@ -2,6 +2,8 @@ import { render, renderHook, screen } from '@testing-library/react';
 import React from 'react';
 import APILayout from '@src/layouts/api-layout';
 
+import '@src/jest/jest-mocks';
+
 describe('API Layout', () => {
     //can be it or test
     it('render without errors', () => {
